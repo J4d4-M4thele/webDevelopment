a550158e36c381f7f5ce524d98fc765347dc19fb
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-09-20 10:05:25 +0200

    update

--- frontend/src/services/movies.js
+++ frontend/src/services/movies.js
@@ -1,34 +1,34 @@
 import axios from "axios";
 
-class MovieDataService{
-
-    // class will contain functions which make the api calls and return the info from the api calls
-    getAll(page = 0){ // default page 0
-        return axios.get(`http://guarded-savannah-47368.herokuapp.com/api/v1/movies?page=${page}`)
+class MovieDataService {
+    getAll(page = 0) {
+        return axios.get(`http://localhost:5000/api/v1/movies?page=${page}`)
     }
-
-    get(id){         
-        return axios.get(`http://guarded-savannah-47368.herokuapp.com/api/v1/movies/id/${id}`)
-    }   
-    
-    // query consist of the actual search title, or ratings e.g. rated=G
-    //CRUD functions for reviews
-    find(query, by = "title", page = 0){
-        return axios.get(`http://guarded-savannah-47368.herokuapp.com/api/v1/movies?${by}=${query}&page=${page}`)
-    }       
-
-    createReview(data){
-        return axios.post("http://guarded-savannah-47368.herokuapp.com/api/v1/movies/review", data)
+    get(id) {
+        return axios.get(`http://localhost:5000/api/v1/movies/id/${id}`)
     }
-    updateReview(data){
-        return axios.put("http://guarded-savannah-47368.herokuapp.com/api/v1/movies/review", data)
+    find(query, by = "title", page = 0) {
+        return axios.get(
+            `http://localhost:5000/api/v1/movies?${by}=${query}&page=${page}`
+        )
     }
-    deleteReview(id, userId){                
-        return axios.delete(`http://guarded-savannah-47368.herokuapp.com/api/v1/movies/review`,{data:{review_id: id, user_id: userId}})        
+    createReview(data) {
+        return axios.post("http://localhost:5000/api/v1/movies/review", data)
     }
-    getRatings(){
-        return axios.get("http://guarded-savannah-47368.herokuapp.com/api/v1/movies/ratings")
-    }    
+    updateReview(data) {
+        return axios.put("http://localhost:5000/api/v1/movies/review", data)
+    }
+    deleteReview(id, userId) {
+        return axios.delete(
+            "http://localhost:5000/api/v1/movies/review",
+            { data: { review_id: id, user_id: userId } }
+        )
+    }
+    getRatings() {
+        return axios.get("http://localhost:5000/api/v1/movies/ratings")
+    }
 }
 
-export default new MovieDataService()
+const movieDataService = new MovieDataService();
+
+export default movieDataService;
