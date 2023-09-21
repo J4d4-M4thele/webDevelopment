d393262ce4e69ec599651aed9f02bc342db0e911
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-09-21 12:57:32 +0200

    update

--- frontend/src/components/add-review.js
+++ frontend/src/components/add-review.js
@@ -64,7 +64,7 @@ const AddReview = props => {
          {submitted ? (
             <div>
                <h4>Review submitted successfully</h4>
-               <Link to={'/movies' + props.match.params.id}>
+               <Link to={'/movies/' + props.match.params.id}>
                   Back to Movie
                </Link>
             </div>
