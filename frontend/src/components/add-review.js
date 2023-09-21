0c03449027f553d6f555b13702cbdc7bcef5ad79
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-09-21 09:39:29 +0200

    update

--- frontend/src/components/add-review.js
+++ frontend/src/components/add-review.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState } from 'react';
+import MovieDataService from "../services/movies";
+import { Link } from "react-router-dom";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-function AddReview() {
-    return(
-       <div className="App">
-          Add Review
-       </div>
-    );
+const AddReview = props => {
+
+   let editing = false
+   let initialReviewState = ''
+
+   const [review, setReview] = useState(initialReviewState)
+   //keeps track if review's submitted
+   const [submitted, setSubmitted] = useState(false)
+
+   const onChangeReview = e => {
+      const review = e.target.value
+      setReview(review);
+   }
+
+   const saveReview = () => {
+      var data = {
+         review: review,
+         name: props.user.name,
+         user_id: props.user.id,
+         //get movie from direct url
+         movie_id: props.match.params.id
+      }
+   }
+   
+   MovieDataService.createReview(data)
+   .then(response => {
+      setSubmitted(true)
+   })
+   .catch(e => {
+      console.log(e)
+   })
+   
+   return (
+      <div className="App">
+         Add Review
+      </div>
+   );
 }
 
 export default AddReview;
